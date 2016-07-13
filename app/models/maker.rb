1cc2bdd59424f924fdd6c7dd6c63f92e65229ea0
Felipe Almeida <felipe.egoista@gmail.com>
2016-07-13 15:07:20 -0300

    improve performance of models batch_update

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -2,8 +2,8 @@ class Maker < ActiveRecord::Base
   has_many :models
 
   def self.batch_update(makers)
-    makers.each do |maker|
-      maker.save unless Maker.exists?(name: maker.name)
-    end
+    persisted_makers = self.all
+    diff_makers = makers.keep_if{ |maker| not persisted_makers.any? { |p_maker| p_maker.name == maker.name }}
+    diff_makers.each{ |maker| maker.save}
   end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -2,8 +2,10 @@ class Model < ActiveRecord::Base
   belongs_to :customer
 
   def self.batch_update(models)
-    models.each do |model|
-      model.save unless Model.exists?(name: model.name, maker_id: model.maker_id)
+    persisted_models = self.all
+    diff_models = models.keep_if do |model|
+      not persisted_models.any? { |p_model| p_model.name == model.name and p_model.maker_id == model.maker_id }
     end
+    diff_models.each{ |model| model.save}
   end
 end
